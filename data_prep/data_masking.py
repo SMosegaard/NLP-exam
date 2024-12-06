1274209c57dcc7c88354f1bfc7ca517f5bf515b6
SMosegaard <sofie.mosegaard@live.dk>
2024-12-06 15:06:21 +0100

    updated. Forgot to add all_female and all_male test

--- data_prep/data_masking.py
+++ data_prep/data_masking.py
@@ -20,7 +20,7 @@ def count_terms(review, terms = all_terms):
     '''
     term_counted = dict.fromkeys(terms, 0)
     for term in terms:
-        term = re.escape(term) # to avoid regex issues
+        #term = re.escape(term) # to avoid regex issues
         pattern = rf'{term}\w*'
         term_counted[term] = len(re.findall(pattern, review))
     return term_counted
@@ -31,7 +31,7 @@ def mask_by_term_dict(review, term_dict):
     Replace gender-specific terms in the review based on the term_dict
     '''
     for term, replacement in term_dict.items():
-        term = re.escape(term)
+        #term = re.escape(term)
         pattern = rf'({term})(\w*)'
         review = re.sub(pattern, replacement + '\\2', review)
     return review
@@ -56,7 +56,7 @@ def make_neutral(review, terms = all_terms):
     Mask all female- and male-specific names and terms in the text to make it neutral.
     '''
     for term in terms:
-        term = re.escape(term)
+        #term = re.escape(term)
         pattern = rf'{term}\w*'
         review = re.sub(pattern, '', review)
     return review
@@ -89,36 +89,32 @@ def split_and_save(df, output_path):
     train_df, test_df = train_test_split(df, test_size = 0.3, random_state = 123)
 
     original_train = train_df[['ID', 'Review_cleaned', 'Sentiment']].rename(columns = {'Review_cleaned': 'text'})
-    original_test = test_df[['ID', 'Review_cleaned', 'Sentiment']].rename(columns = {'Review_cleaned': 'text'})
 
     neutral_train = train_df[['ID', 'text_all_neutral', 'Sentiment']].rename(columns = {'text_all_neutral': 'text'})
-    neutral_test = test_df[['ID', 'text_all_neutral', 'Sentiment']].rename(columns = {'text_all_neutral': 'text'})
 
     mix_train = pd.concat([
         train_df[['ID', 'text_all_male', 'Sentiment']].rename(columns = {'text_all_male': 'text'}),
         train_df[['ID', 'text_all_female', 'Sentiment']].rename(columns = {'text_all_female': 'text'})])
 
-    mix_test = pd.concat([
-        test_df[['ID', 'text_all_male', 'Sentiment']].rename(columns={'text_all_male': 'text'}),
-        test_df[['ID', 'text_all_female', 'Sentiment']].rename(columns={'text_all_female': 'text'})])
+    all_male_test = test_df[['ID', 'text_all_male', 'Sentiment']].rename(columns = {'text_all_male': 'text'})
+    all_female_test = test_df[['ID', 'text_all_female', 'Sentiment']].rename(columns = {'text_all_female': 'text'})
 
     original_train.to_csv(os.path.join(output_path, "original_train.csv"), index = False)
-    original_test.to_csv(os.path.join(output_path, "original_test.csv"), index = False)
     neutral_train.to_csv(os.path.join(output_path, "neutral_train.csv"), index = False)
-    neutral_test.to_csv(os.path.join(output_path, "neutral_test.csv"), index = False)
     mix_train.to_csv(os.path.join(output_path, "mix_train.csv"), index = False)
-    mix_test.to_csv(os.path.join(output_path, "mix_test.csv"), index = False)
+    all_male_test.to_csv(os.path.join(output_path, "all_male_test.csv"), index = False)
+    all_female_test.to_csv(os.path.join(output_path, "all_female_test.csv"), index = False)
 
 
 def main():
 
-    filepath = "/work/SofieNørboMosegaard#5741/NLP/NLP-exam/data/reviews.csv"
+    filepath = "/work/SofieNørboMosegaard#5741/NLP/NLP-exam/data_2/cleaned_reviews.csv"
     #filepath = "data/reviews.csv'"
+
     df = pd.read_csv(filepath)
+    df = df[df.Sentiment != 'neu'] # only interested reviews with pos and neg sentiment
 
-    df = df[:10]
-
-    output_path = "/work/SofieNørboMosegaard#5741/NLP/NLP-exam/data/reviews_masked.csv"
+    output_path = "/work/SofieNørboMosegaard#5741/NLP/NLP-exam/data_2/"
     #outpath = "data/reviews_masked.csv"
     masked_df = make_all_df(df, output_path)
 
